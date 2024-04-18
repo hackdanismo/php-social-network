442b32829ebaccf845a1720622925d89d4e11dbc
hackdanismo <office@justtoconfirm.co.uk>
2024-04-18 13:37:23 +0100

    feat: create initial connection the the MySQL database using PHP PDO

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-  echo "PHP Social Network";
+  //echo "PHP Social Network";
+
+  $dbHost = "localhost";
+  $dbUsername = "username";
+  $dbPassword = "password";
+  $dbDatabase = "php_social_db";
+
+  try {
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbDatabase", $dbUsername, $dbPassword);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "++ Database connected successfully ++";
+  } catch(PDOException $e) {
+    echo "++ Connection failed: " . $e->getMessage();
+  }
